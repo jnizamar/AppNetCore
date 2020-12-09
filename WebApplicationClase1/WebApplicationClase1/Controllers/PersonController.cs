f1da82b4dd837881eb0e28256577b56a61db34d6
unknown <jnizama@SERVER01.horizont.net>
2020-12-09 18:28:03 -0500

    mi primer commit

---
+++ WebApplicationClase1/WebApplicationClase1/Controllers/PersonController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using WebApplicationClase1.Models;
+
+namespace WebApplicationClase1.Controllers
+{
+    public class PersonController : Controller
+    {
+        public IActionResult Index()
+        {
+            var persons = new List<Person>();
+            persons.Add(new Person { 
+                Id = 1,
+                Nombre = "Juan",
+                Apellido = "Nizama"
+            });
+            return View(persons);
+        }
+    }
+}
---
+++ WebApplicationClase1/WebApplicationClase1/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace WebApplicationClase1.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
